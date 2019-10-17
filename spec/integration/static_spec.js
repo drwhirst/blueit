fc4d7ae8c05e70e7582ba8397439d12422afb1b4
David Hirst <dhirst@ametroslearning.com>
2019-10-17 10:03:34 -0400

    adds http test for gt for get landing page

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -2,7 +2,7 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
-describe("routes L static", () => {
+describe("routes : static", () => {
     describe("GET /", () => {
         it("should return status code 200", (done) => {
             request.get(base, (err,res, body) => {
