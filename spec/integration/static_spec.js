8e5793095743ab9f1099e8cfb64952df7c2168cd
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-10-21 13:22:59 -0400

    adds about view and route

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,11 +5,21 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
     describe("GET /", () => {
         it("should return status code 200 and have 'Welcome to Blueitt! in the body of the response", (done) => {
-            request.get(base, (err,res, body) => {
+            request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(body).toContain("Welcome to Blueitt!")
                 done();
             });
         });
+    });
+
+    describe("GET /about", () => {
+        it("Should return status code 200 and contain the string, 'About Us' in the body of the response", (done) => {
+            request.get(`${base}about`, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain('About Us.');
+                done();
+            })
+        })
     });
 });
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,5 +1,9 @@
 module.exports = {
     index(req,res, next) {
         res.render("static/index", {title: "Welcome to Blueitt!"});
-    }
+    },
+
+    about(req, res, next) {
+        res.render("static/about", {title: "About Blueitt."})
+    },
 }
