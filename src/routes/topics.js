3cb1a671960c5dc40ba49defc2fa97d4c43547e5
Drwhirstyburgers <dave.hirst50@gmail.com>
2019-10-23 18:44:59 -0400

    adds topic create

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const topicsController = require("../controllers/topicController");
 
 router.get("/topics", topicsController.index);
+router.get("/topics/new", topicsController.new);
+router.post("/topics/create", topicsController.create);
 
 module.exports = router;
