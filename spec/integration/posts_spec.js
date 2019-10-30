0c82a055bf739b4cbca72e60fbacdcab4c10899e
David Hirst <dhirst@ametroslearning.com>
2019-10-30 11:21:11 -0400

    adds beforeEach on integration tests for posts]

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -7,5 +7,32 @@ const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
 
 describe("routes : posts", () => {
+    beforeEach((done) => {
+        this.topic;
+        this.post;
+        sequelize.sync({force: true}).then((res) => {
+            Topic.create({
+                title: "Did OJ do it?",
+                description: "A place to review the evidence and plead your case."
+            })
+            .then((topic) => {
+                this.topic = topic;
+                Post.create({
+                    title: "If the glove fits...",
+                    body: "Then he probably did it.",
+                    topicId: this.topic.id
+                })
+                .then((post) => {
+                    this.post = post;
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            });
+        });
+    });
+
     
 });
